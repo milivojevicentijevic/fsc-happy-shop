d4e2cb2b3ddda543f524bd208b2df06e6b53f7ad
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-05-19 19:17:33 +0200

    topbar

--- index.php
+++ index.php
@@ -5,10 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Happy shop</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Happy shop</h1>
+    <!-- topbar -->
+    <section class="topbar">
+        <article class="container">
+            <div class="row align-items-center">
+                <div class="col-md-4">
+                    <span class="text-white">Call support: 062 218 454</span>
+                </div>
+                <div class="col-md-8">
+                    <ul class="nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Superdeals</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Featured brands</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Best selling</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tech discovery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Trending styles</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 </body>
 </html>
